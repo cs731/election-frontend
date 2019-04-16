172e241045402603f9494294e51a2950c579fce0
pardhu225 <chowdary1969@gmail.com>
2019-04-16 23:30:44 +0530

    done bc

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { CastVoteComponent } from './components/cast-vote/cast-vote.component';
+import { SuccessfulVoteComponent } from './components/successful-vote/successful-vote.component';
+import { FailedVoteComponent } from './components/failed-vote/failed-vote.component';
 
 const routes: Routes = [
-  {path: '', component: CastVoteComponent}
+  {path: '', component: CastVoteComponent},
+  {path: 'success', component: SuccessfulVoteComponent},
+  {path: 'fail', component: FailedVoteComponent},
+
 ];
 
 @NgModule({
